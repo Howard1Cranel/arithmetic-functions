4bc5f68de713e8b6c1db637abcd74be8ca8585a4
Romanov Matvey <143862938+Howard1Cranel@users.noreply.github.com>
2023-09-27 15:17:58 +0700

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+import math
+
+a1 = float(input("Введите a1: "))
+a2 = float(input("Введите a2: "))
+
+
+z1 = 2 * (2 ** math.sin(3 * math.pi - 2 * a1)) * (2 ** math.cos(5 * math.pi + 2 * a1))
+z2 = (1 / 4) - (1 / 4) * math.sin((5 / 2) * math.pi - 8 * a2 )
+
+print(f"Вывод a1: {z1}")
+print(f"Вывод a1: {z2}")
